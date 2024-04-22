93685252f6718a2d4302e9ed3a838cdae88fc47b
huanghitoy <huanghitoy@gmail.com>
2024-04-22 20:22:21 +0800

    Update diy-Lienol.sh

--- diy-Lienol.sh
+++ diy-Lienol.sh
@@ -14,6 +14,7 @@ rm -rf feeds/packages/net/{xray-*,trojan*,v2ray-*,sing*}
 rm -rf feeds/other/lean/{luci-app-qbittorrent,qBittorrent*,qtbase,qttools,rblibtorrent}
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/lang/golang
+git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/luci-app-alist package/alist
 git clone https://github.com/sbwml/openwrt-qBittorrent package/qBittorrent
