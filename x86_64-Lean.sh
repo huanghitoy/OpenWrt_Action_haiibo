32f15e537c8449b4c56c85544e022074c5935435
huanghitoy <huanghitoy@gmail.com>
2024-04-27 21:05:58 +0800

    Update x86_64-Lean.sh

--- x86_64-Lean.sh
+++ x86_64-Lean.sh
@@ -29,23 +29,22 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 添加额外插件 原来包和kenzo包没有的 5个
-rm -rf package/{luci-app-poweroff,OpenAppFilter,luci-app-netdata,luci-app-ssr-mudb-server,luci-app-wolplus}
+# 添加额外插件 原来包和kenzo包没有的 4个
+rm -rf package/{luci-app-poweroff,OpenAppFilter,luci-app-netdata,luci-app-wolplus}
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git clone --depth=1 https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-ssr-mudb-server
 
-# 移除kenzo中包，添加其他包 1个
+
+# 移除kenzo中包1个，添加其他包 2个
 rm -rf feeds/kenzo/luci-app-fileassistant
-rm -rf package/luci-app-fileassistant
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-fileassistant
+rm -rf package/{luci-app-fileassistant，luci-app-ssr-mudb-server}
+git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-fileassistant luci-app-ssr-mudb-server
 #git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 #git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 #git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-#git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 #git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 
 # msd_lite
