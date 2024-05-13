0a9245cb7237ae805da90fea755b21880c72e7fd
huanghitoy <huanghitoy@gmail.com>
2024-05-13 16:35:32 +0800

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 REPO_URL="https://github.com/huanghitoy/openwrt"
-REPO_BRANCH="v21.02.7"
-git clone -b $REPO_BRANCH $REPO_URL $REPO_BRANCH
+REPO_BRANCH="openwrt-21.02"
+git clone -b $REPO_BRANCH --depth=1 $REPO_URL $REPO_BRANCH
 cd $REPO_BRANCH
+
 ./scripts/feeds update -a
 ./scripts/feeds install  -a
