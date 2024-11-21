2268d9a4194e58377d5e12f5dd027b262af3e351
huanghitoy <huanghitoy@gmail.com>
2024-11-21 21:55:12 +0800

    Update openwrt-23.05.sh

--- openwrt-23.05.sh
+++ openwrt-23.05.sh
@@ -16,6 +16,7 @@ sed -i 's/192.168.1.1/192.168.12.199/g' package/base-files/files/bin/config_gene
 
 # 修改默认时区
 sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
+
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
