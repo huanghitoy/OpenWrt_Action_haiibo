36a235efb1aada4c5c68e4e6abfe41a6cbbd9b71
huanghitoy <huanghitoy@gmail.com>
2025-09-07 09:46:02 +0800

    Delete openwrt.sh

--- openwrt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPO_URL="https://github.com/huanghitoy/openwrt"
-REPO_BRANCH="openwrt-21.02"
-git clone -b $REPO_BRANCH --depth=1 $REPO_URL $REPO_BRANCH
-cd $REPO_BRANCH
-
-./scripts/feeds update -a
-./scripts/feeds install  -a
