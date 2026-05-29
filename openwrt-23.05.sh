eac5d08e3647efcd13d186049a15dc6d44ee382f
huanghitoy <huanghitoy@gmail.com>
2026-05-29 11:37:57 +0800

    Update openwrt-23.05.sh

--- openwrt-23.05.sh
+++ openwrt-23.05.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #openwrt-23.05
 
-# 新增这一行，强制锁定到 openwrt 根目录，模拟以前正常的环境
-#cd "$GITHUB_WORKSPACE/openwrt" || exit 1
 
 #sed -i 's/ +libopenssl-legacy//g' feeds/small/shadowsocksr-libev/Makefile
 # 取消默认主题luci-theme-bootstrap  
@@ -35,8 +33,8 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 #rm -rf feeds/packages/net/{mosdns,adguardhome,pdnsd-alt,smartdns,v2ray-geodata,msd_lite}
 #rm -rf feeds/luci/applications/{luci-app-serverchan,luci-app-aliyundrive-webdav,luci-app-argon-config,luci-app-design-config,luci-app-dockerman,luci-app-easymesh,luci-app-eqos,luci-app-smartdns,luci-app-mosdns,luci-app-netdata}
 #rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-design}
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Git稀疏克隆，只克隆指定目录到本地
 rm -rf package/huang
@@ -141,8 +139,15 @@ git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config
 #git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
 # MosDNS
-rm -rf package/luci-app-mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/luci-app-mosdns
+#requires golang 1.25.x or latest version
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+# remove v2ray-geodata package from feeds
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
 
 # SmartDNS
 #rm -rf openwrt/feeds/luci/applications/luci-app-smartdns
