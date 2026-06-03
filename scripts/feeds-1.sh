e7eccfd17d618845d6cb1bdea792d3a67a316381
huanghitoy <huanghitoy@gmail.com>
2026-06-04 06:59:10 +0800

    Update feeds-1.sh

--- scripts/feeds-1.sh
+++ scripts/feeds-1.sh
@@ -12,24 +12,25 @@ PASSWALL_LUCI_FEEDS_COMMITS="46e926363e900974994f6c0311768db599574b02"
 #PASSWALL_PACKAGES_FEEDS_COMMITS="f002d6d83f0e2f21b7db251410409eb472fd2d6e"
 #PASSWALL_LUCI_FEEDS_COMMITS="d13c49df62631f3fecd9d3146e370b419f5ed049"
 
-#passwall 25.12.31  
+#passwall 25.12.31
 #PASSWALL_PACKAGES_FEEDS_COMMITS="46495fc982f7861e8913b8667bfdcd523b7ec2fc"
 #PASSWALL_LUCI_FEEDS_COMMITS="d743aeeeeced58359cd066ed5679985c5c82c97c"
 
 
-#sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main\nsrc-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git^'$PASSWALL_PACKAGES_FEEDS_COMMITS'\nsrc-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git^'$PASSWALL_LUCI_FEEDS_COMMITS'' feeds.conf.default
 sed -i 's/^.*telephony.git.*$/src-git telephony https:\/\/github.com\/hitoyhuang\/telephony.git;openwrt-23.05/' feeds.conf.default
-#sed -i 's/^.*telephony.git.*$/src-git telephony https:\/\/github.com\/koreapyj\/telephony.git^8a0f6d84d7a4340098f5520e8d6fc5485b8fd995/' feeds.conf.default
-#sed -i 's/^.*telephony.git.*$/src-git telephony https:\/\/github.com\/huanghitoy\/telephony.git;openwrt-22.03/' feeds.conf.default
 
 ./scripts/feeds update -a
 
+# ====================== 【关键修复：位置放对了】======================
+# 修复 xray-plugin 下载失败 + 哈希错误
+sed -i 's|PKG_SOURCE_URL:=.*codeload.github.com.*|PKG_SOURCE_URL:=https://github.com/teddysun/xray-plugin/archive/refs/tags/|' feeds/passwall_packages/xray-plugin/Makefile
+sed -i 's|PKG_HASH:=.*|PKG_HASH:=1150968f8791df884ce0ab5b2dbc870496088c90b5ffcc7f21497075aab7b1b5|' feeds/passwall_packages/xray-plugin/Makefile
+# ====================================================================
+
 ./scripts/feeds install -a
-
-
 
 wget -O -  https://github.com/raphikWasHere/bluealsa4openwrt/raw/refs/heads/main/bluez-alsa/packages/full.tar.gz | tar -zxvf - -C ./
 
 ./scripts/feeds update
-./scripts/feeds install bluez-alsa  
+./scripts/feeds install bluez-alsa
