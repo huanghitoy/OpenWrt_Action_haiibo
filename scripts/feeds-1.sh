9a7a234186be0a6324ccb9010f228debfa1afdf1
huanghitoy <huanghitoy@gmail.com>
2026-06-05 08:40:30 +0800

    Update feeds-1.sh

--- scripts/feeds-1.sh
+++ scripts/feeds-1.sh
@@ -13,7 +13,7 @@ sed -i 's/^.*telephony.git.*$/src-git telephony https:\/\/github.com\/hitoyhuang
 
 ./scripts/feeds update -a
 
-# ✅ 修复 哈希值不匹配（你日志里的真实错误）
+# ✅ 修复 xray-plugin-1.8.24 在passwall-25.7.15版本中哈希值不匹配，在最新版中已修复
 sed -i 's|PKG_HASH:=.*|PKG_HASH:=1150968f8791df884ce0ab5b2dbc870496088c90b5ffcc7f21497075aab7b1b5|g' feeds/passwall_packages/xray-plugin/Makefile
 
 ./scripts/feeds install -a
