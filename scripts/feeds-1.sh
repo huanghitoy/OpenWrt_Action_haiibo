9a11fbbb14158664dbcba61848da713c7c0cb5be
huanghitoy <huanghitoy@gmail.com>
2026-07-30 17:35:14 +0800

    Update feeds-1.sh

--- scripts/feeds-1.sh
+++ scripts/feeds-1.sh
@@ -1,4 +1,9 @@
+
 #!/bin/bash
+
+##passwall 25.5.16  可以
+#PASSWALL_PACKAGES_FEEDS_COMMITS="1013e7cb1dca5c0835ca277a1f80c2f81549be42"
+#PASSWALL_LUCI_FEEDS_COMMITS="74f7fa3e3279b4cef9471cd7bccec310a10dcb74"
 
 #passwall-25.7.15  xray-core-25.7.26  23.05,24.10 编译通过可以
 #PASSWALL_PACKAGES_FEEDS_COMMITS="950c9a23581ed4cfdcc71a03395213f92ea85f8a"
@@ -7,22 +12,28 @@
 #passwall-25.12.1  xray-core-25.12.8        23.05 编译通过可以
 PASSWALL_PACKAGES_FEEDS_COMMITS="b37a3f1ce3512b61143c4fa49335a074d197bcf5"
 PASSWALL_LUCI_FEEDS_COMMITS="d73f09db0462a65cbfe989b2b8d41fc0bac2b008"
-PASSWALL2_LUCI_FEEDS_COMMITS="4584a89c749c5a4f56c04f720937df27357a81be"
+
+#passwall 25.12.31  
+#PASSWALL_PACKAGES_FEEDS_COMMITS="46495fc982f7861e8913b8667bfdcd523b7ec2fc"
+#PASSWALL_LUCI_FEEDS_COMMITS="d743aeeeeced58359cd066ed5679985c5c82c97c"
 
 
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git^'$PASSWALL_PACKAGES_FEEDS_COMMITS'\nsrc-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git^'$PASSWALL_LUCI_FEEDS_COMMITS'' feeds.conf.default
-#sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git^'$PASSWALL_PACKAGES_FEEDS_COMMITS'\nsrc-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git^'$PASSWALL_LUCI_FEEDS_COMMITS'\nsrc-git passwall2_luci https://github.com/Openwrt-Passwall/openwrt-passwall2.git^'$PASSWALL2_LUCI_FEEDS_COMMITS'' feeds.conf.default
 
-
-#sed -i 's/^.*telephony.git.*$/src-git telephony https:\/\/github.com\/hitoyhuang\/telephony.git;openwrt-23.05/' feeds.conf.default
 echo "src-git asterisk_chan_quectel https://github.com/huanghitoy/openwrt-asterisk-chan-quectel.git;master" >> feeds.conf.default
+echo "src-git asterisk_chan_quectel5g https://github.com/hitoyhuang/openwrt-asterisk-chan-quectel5g.git;master" >> feeds.conf.default
 
 echo "src-git kms https://github.com/gaoderby/luci-app-kms.git" >> feeds.conf.default
+
+# MediaMTX (原rtsp-simple-server) 流媒体网关
+#echo "src-git mediamtx https://github.com/huanghitoy/mediamtx-openwrt-feed.git" >> feeds.conf.default
+# libcamera 摄像头驱动库
+#echo "src-git libcamera https://github.com/huanghitoy/libcamera-openwrt-feed.git" >> feeds.conf.default
 
 
 ./scripts/feeds update -a
 
-# ✅ 修复 xray-plugin-1.8.24 在passwall-25.7.15版本中哈希值不匹配，在最新版中已修复
+# ✅ 修复 xray-plugin-1.8.24 在passwall-25.12.5之前版本中哈希值不匹配，在最新版中已修复
 sed -i 's|PKG_HASH:=.*|PKG_HASH:=1150968f8791df884ce0ab5b2dbc870496088c90b5ffcc7f21497075aab7b1b5|g' feeds/passwall_packages/xray-plugin/Makefile
 
 ./scripts/feeds install -a
